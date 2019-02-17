01d2c8b36235dd167cda8b7515f839ed3e3edefe
jejewitt <jejewitt@ualberta.ca>
2019-02-17 16:59:35 -0700

    added files

---
+++ react-weather-app/build/precache-manifest.3605445f421c207abf7818e000d85fb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82770e04e5a2b8622e2f",
+    "url": "/static/js/main.82770e04.chunk.js"
+  },
+  {
+    "revision": "947d507a2eb31f2f56b4",
+    "url": "/static/js/1.947d507a.chunk.js"
+  },
+  {
+    "revision": "82770e04e5a2b8622e2f",
+    "url": "/static/css/main.32c84d90.chunk.css"
+  },
+  {
+    "revision": "13902634d00db0528d1e03aa49bd35d6",
+    "url": "/index.html"
+  }
+];
